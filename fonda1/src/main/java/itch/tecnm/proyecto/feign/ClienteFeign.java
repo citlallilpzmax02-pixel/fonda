9e980a8b01a0ee8f7ded05f119d813793238c78f
Citlalli lopez <amayranilpz09@gmail.com>
2025-12-05 10:31:39 -0600

    version6

--- fonda1/src/main/java/itch/tecnm/proyecto/feign/ClienteFeign.java
+++ fonda1/src/main/java/itch/tecnm/proyecto/feign/ClienteFeign.java
@@ -15,7 +15,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 public interface ClienteFeign {
 
-    @GetMapping("https://restaurante-production-1b90.up.railway.app/api/cliente/{id}")
+    @GetMapping("/api/cliente/{id}")
     ClienteDto getClienteById(@PathVariable("id") Integer idCliente);
 
     // =============================
