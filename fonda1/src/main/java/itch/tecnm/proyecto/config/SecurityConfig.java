db4d98a08ea618d0018164b68fa349ba3047302c
Citlalli lopez <amayranilpz09@gmail.com>
2025-12-05 08:18:52 -0600

    version2

--- fonda1/src/main/java/itch/tecnm/proyecto/config/SecurityConfig.java
+++ fonda1/src/main/java/itch/tecnm/proyecto/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(java.util.List.of("http://localhost:3000"));
+        config.setAllowedOrigins(java.util.List.of("https://frontend-production-5988.up.railway.app"));
         config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(java.util.List.of("*"));
         config.setExposedHeaders(java.util.List.of("Authorization"));
